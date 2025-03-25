9f1bff207c564f06cc430588d44b59047ab7daff
t184914 <tor.tsn@gmail.com>
2025-03-25 13:14:01 +0100

    p

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,10 @@
 module Oblig3_dat107 {
+	requires eclipselink;
+	requires jakarta.persistence;
+	requires java.desktop;
+	requires java.xml;
+	requires jdk.compiler;
+	requires jdk.javadoc;
+	requires jdk.jfr;
+	requires jdk.jpackage;
 }
